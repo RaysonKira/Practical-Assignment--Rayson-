07612fe7483c981bbd4edfc839e0283f547869bd
Rayson (See Differently) <32917743+RaysonKira@users.noreply.github.com>
2018-11-12 21:32:14 +0800

    push 2

---
+++ MovieRater3 copy/app/src/main/java/com/nyp/sit/dit/it2107/movierater/rateMovie.kt
@@ -0,0 +1,15 @@
+package com.nyp.sit.dit.it2107.movierater
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class rateMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.rate_movie)
+    }
+}
